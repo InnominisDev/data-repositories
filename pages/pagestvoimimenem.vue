514836bbbcbc6cb9a7eba7f421c01c64c01a9963
Timofey <kit.bonus@ya.ru>
2020-07-28 10:53:45 +0300

    append the Page

---
+++ pages/pagestvoimimenem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="malevich"></div>
+</template>
+
+<script>
+
+export default {
+
+    mounted ()
+    {
+        var el = document.getElementById('malevich');
+        el.style.top = window.innerHeight/2 - 5 + 'px';
+        el.style.left = window.innerWidth/2 - 5 + 'px';
+    }
+
+}   
+</script>
+
+<style>
+
+#malevich {
+    position: relative;
+    background: black;
+    width: 10px;
+    height: 10px;
+}
+</style>
